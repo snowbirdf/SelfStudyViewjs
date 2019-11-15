ff4dc2794126afd5228827cce0640ebdb4733605
snowbird <snowbird.and.townie+github@gmail.com>
2019-11-15 15:23:12 +0900

    slot scope sample

---
+++ ch5/slotScope/app.js
@@ -0,0 +1,23 @@
+var MyButton = {
+    data: function () {
+        return {
+            textLabel: 'child'
+        }
+    },
+    template: `
+        <button>
+            <slot>OK</slot>
+        </button>
+    `
+}
+new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            textLabel: 'parent'
+        }
+    },
+    components: {
+        MyButton: MyButton
+    }
+})
