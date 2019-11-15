90a7e95d09a61df4ced40d573b11b4a3cc4f9c6d
snowbird <snowbird.and.townie+github@gmail.com>
2019-11-15 21:30:44 +0900

    change component tsmplate to render function

--- ch5/drawingFunction/app.js
+++ ch5/drawingFunction/app.js
@@ -1,16 +1,13 @@
 var MyButton = {
     props: ['href', 'tag'],
-    template: `
-        <a v-if="(!tag && href) || tag ==='a'" :href="href || '#'">
-            <slot></slot>
-        </a>
-        <span v-else-if="tag ==='span'">
-            <slot></slot>
-        </span>
-        <button v-else>
-            <slot></slot>
-        </button>
-   `
+    render: function (createElement) {
+        var tag = this.tag || (this.href ? 'a' : 'button')
+        return createElement(tag, {
+            attrs: {
+                href: this.href || '#'
+            }
+        }, this.$slots.default)
+    }
 }
 
 new Vue({
