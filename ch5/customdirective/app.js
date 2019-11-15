bb76983ec46bf59a6ae8eaa59e542406a6912349
snowbird <snowbird.and.townie+github@gmail.com>
2019-11-15 20:06:08 +0900

    add modifire 'once'

--- ch5/customdirective/app.js
+++ ch5/customdirective/app.js
@@ -1,11 +1,28 @@
 Vue.directive('fallback-image', {
-    bind: function (el) {
-        el.addEventListener('error', function () {
-            el.src = 'https://dummyimage.com/400x400/000/ffff.png&text=no+image'
+    bind: function (el, binding) {
+        console.log('bind', binding)
+        var once = binding.modifiers.once
+        el.addEventListener('error', function onError() {
+            el.src = binding.value
+            if (once) {
+                el.removeEventListener('error', onError)
+            }
         })
+    },
+    update: function (el, binding) {
+        console.log('update', binding)
+        if (binding.oldValue !== binding.value && binding.oldValue === el.src) {
+            el.src = binding.value
+        }
     }
 })
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: function () {
+        return {
+            altText: 'logo',
+            noImageURL: 'https://dummyimage.com/400x400/000/ffffff.png&text=no+image'
+        }
+    }
 })
