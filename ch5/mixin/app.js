a39a48d24e1d6d0afb1b7085e03c36d852fc9e55
snowbird <snowbird.and.townie+github@gmail.com>
2019-11-16 21:24:38 +0900

    extract mixin

--- ch5/mixin/app.js
+++ ch5/mixin/app.js
@@ -1,7 +1,5 @@
-var IconShareButton = {
-    template: `
-        <button @click="share"><i class="fas fa-share-square"</i></button>
-    `,
+//mixin
+var Sharable = {
     data: function () {
         return {
             _isProcessing: false
@@ -24,32 +22,23 @@ var IconShareButton = {
     }
 }
 
+var IconShareButton = {
+    mixins: [Sharable],
+    template: `
+        <button @click="share"><i class="fas fa-share-square"</i></button>
+    `,
+}
+
 var TextShareButton = {
+    mixins: [Sharable],
     template: `
         <button @click="share">{{buttonLabel}}</button>
     `,
     data: function () {
         return {
-            buttonLabel: 'シェアする',
-            _isProcessing: false
+            buttonLabel: 'シェアする'
         }
     },
-    methods: {
-        share: function () {
-            if (this._isProcessing) {
-                return
-            }
-            if (!window.confirm('シェアしますか？')) {
-                return
-            }
-            this._isProcessing = true
-
-            setTimeout(() => {
-                window.alert('シェアしました')
-                this._isProcessing = false
-            }, 3000)
-        }
-    }
 }
 
 new Vue({
