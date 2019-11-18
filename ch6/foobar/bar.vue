99c570fc5a1bc8b017e2dbccfc70673ca5e2dbe0
snowbird <snowbird.and.townie+github@gmail.com>
2019-11-18 13:17:58 +0900

    child component and style scope sample

---
+++ ch6/foobar/bar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはbarコンポーネントです。</p>
+    <foo />
+  </div>
+</template>
+
+<script>
+import Foo from "./foo";
+export default {
+  components: { Foo }
+};
+</script>
+
+<style scoped>
+.bar {
+  border: 1px solid red;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 125%;
+}
+</style>
---
+++ ch6/foobar/foo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="foo">
+    <h1 class="header">Fooコンポーネントです</h1>
+    <p>これはFooコンポーネントです</p>
+  </div>
+</template>
+
+<style>
+p {
+  text-decoration: underline;
+}
+</style>
+
+<style scoped>
+.foo {
+  border: solid 1px green;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 150%;
+}
+</style>
+
+
+
+
+
