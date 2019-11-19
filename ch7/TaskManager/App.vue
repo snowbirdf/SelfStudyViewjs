b2036a01aa2fd825161b64e9cf8b35f6b95fa2f6
snowbird <snowbird.and.townie+github@gmail.com>
2019-11-19 10:11:35 +0900

    display tasks

--- ch7/TaskManager/App.vue
+++ ch7/TaskManager/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done" />
+        {{task.name}}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    }
+  }
+};
 </script>
