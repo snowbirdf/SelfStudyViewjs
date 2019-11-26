4a8fc5507eb2e17d4ae3fad189cd152388823ecf
akikokomura <akikokomura@akikokomuranoMacBook-Pro.local>
2019-11-26 21:32:31 +0900

    setting polyfill in unit test config

--- ch8/kanban-app/test/unit/karma.conf.js
+++ ch8/kanban-app/test/unit/karma.conf.js
@@ -5,7 +5,7 @@
 
 var webpackConfig = require('../../build/webpack.test.conf')
 
-module.exports = function karmaConfig (config) {
+module.exports = function karmaConfig(config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
@@ -14,7 +14,9 @@ module.exports = function karmaConfig (config) {
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: ['../../.node_modules/es6-promise/dist/es6-promise.auto.js',
+      './index.js'
+    ],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
