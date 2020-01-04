9f5f68540f846ef4dc168bee2b1ede716d804006
akikokomura <akikokomura@akikokomuranoMacBook-Pro.local>
2020-01-04 22:48:11 +0900

    エラーを修正

--- ch8/kanban-app/src/main.js
+++ ch8/kanban-app/src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-Vue, component(ErrorBoundary.name, ErrorBoundary)
+Vue.component(ErrorBoundary.name, ErrorBoundary)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
